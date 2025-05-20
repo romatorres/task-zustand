de671b6a4f2b2726f72b1ae51140a6a2888154d4
romatorres <romatorres12@gmail.com>
2025-05-20 17:51:10 -0300

    ui header

--- src/app/task/headerTask.tsx
+++ src/app/task/headerTask.tsx
@@ -8,6 +8,11 @@ export default function HeaderTask() {
   const [newTask, setNewTask] = useState("");
   const { addTask } = useTaskStore();
 
+  const { tasks } = useTaskStore();
+
+  const completedTasks = tasks.filter((task) => task.done).length;
+  const pendingTasks = tasks.length - completedTasks;
+
   /* Envia o evento do formulário para adicionar uma nova tarefa, e evita enviar uma tarefa vazia. */
   const handleAddTask = (e: FormEvent) => {
     e.preventDefault();
@@ -18,9 +23,24 @@ export default function HeaderTask() {
 
   return (
     <div>
-      <header className="flex justify-center items-center gap-4 bg-linear-to-r from-primary to-secondary text-white py-6">
-        <CheckCheck size={58} />
-        <h1>Suas Tarefas!</h1>
+      <header className="flex justify-between items-center gap-4 bg-linear-to-r from-primary to-secondary text-white py-6 px-20">
+        <div className="flex items-center gap-4">
+          <CheckCheck size={58} />
+          <h1>Suas Tarefas!</h1>
+        </div>
+        <div className="flex items-center gap-6">
+          <p>
+            Total de tarefas: <span>{tasks.length}</span>
+          </p>
+          <p>
+            Tarefas pendentes:{" "}
+            <span className="pendingTasks">{pendingTasks}</span>
+          </p>
+          <p>
+            Tarefas finalizadas:{" "}
+            <span className="completedTasks">{completedTasks}</span>
+          </p>
+        </div>
       </header>
       <div className="mx-auto max-w-3/5 mt-6">
         <form
--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -6,36 +6,20 @@ import TaskItem from "./item";
 import { useTaskStore } from "../../stores/taskStore";
 
 export default function TaskList() {
-  const { tasks, fetchTasks } = useTaskStore();
+  const { fetchTasks } = useTaskStore();
 
   useEffect(() => {
     fetchTasks();
   }, [fetchTasks]);
-
-  const completedTasks = tasks.filter((task) => task.done).length;
-  const pendingTasks = tasks.length - completedTasks;
 
   return (
     <main>
       <div className="fixed top-0 w-full z-20">
         <HeaderTask />
       </div>
-      <div className="mt-60 z-10">
+      <div className="mt-60 mb-10 z-10">
         <TaskItem />
       </div>
-      <footer>
-        <p>
-          Total de tarefas: <span>{tasks.length}</span>
-        </p>
-        <p>
-          Tarefas pendentes:{" "}
-          <span className="pendingTasks">{pendingTasks}</span>
-        </p>
-        <p>
-          Tarefas finalizadas:{" "}
-          <span className="completedTasks">{completedTasks}</span>
-        </p>
-      </footer>
     </main>
   );
 }
